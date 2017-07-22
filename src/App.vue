85d11362838591d62fd214713f437e0add9ed0e7
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2017-07-22 13:09:39 -0700

    Add vuex with store

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,31 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+  <div id="app" class="container">
+    <div class="row">
+      <div class="col-xs-8 col-xs-offset-2">
+        <h1 class="text-center">Vue-Unit-Test</h1>
+        <hr>
+        <ul>
+          <li v-for="task in tasks" v-text="task"></li>
+        </ul>
+        <hr>
+        <tasks-form></tasks-form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  import {mapGetters} from 'vuex'
+  import TasksForm from './components/TasksForm.vue'
+  export default {
+    name: 'app',
+    components: {
+      TasksForm
+    },
+    computed: {
+      ...mapGetters({
+        tasks: 'tasks'
+      })
+    }
+  }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/TasksForm.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h2 class="text-center">Tasks Form</h2>
+    <form v-on:submit.prevent="onSubmit">
+      <div class="form-group">
+        <label for="task" class="labael-control">Enter New Task</label>
+        <input type="text" class="form-control" id="task" v-model="task">
+      </div>
+      <input type="submit" class="btn btn-default" value="Submit">
+    </form>
+  </div>
+</template>
+
+<script>
+  import { mapActions } from 'vuex'
+
+  export default {
+    name: 'tasks-form',
+    data () {
+      return {
+        task: ''
+      }
+    },
+    methods: {
+      ...mapActions({
+        addTask: 'addTask'
+      }),
+      onSubmit: function () {
+        this.addTask(this.task)
+        this.task = ''
+      }
+    }
+  }
+</script>
